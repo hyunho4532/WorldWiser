8151bfb898774befd522d9830124d1b897cadf98
hyunho4532 <kyounguk192@gmail.com>
2023-12-23 23:14:40 +0900

    2023-12-23 로그인 페이지 구현 완료

--- app/src/main/java/com/hyun/worldwiser/ui/login/LoginActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/login/LoginActivity.kt
@@ -1,12 +1,39 @@
 package com.hyun.worldwiser.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.hyun.worldwiser.R
+import com.hyun.worldwiser.databinding.ActivityLoginBinding
+import com.hyun.worldwiser.ui.login.verification.VerificationActivity
+import com.hyun.worldwiser.util.IntentFilter
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginBinding: ActivityLoginBinding
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val intentFilter: IntentFilter = IntentFilter()
+
+    private val context: Context = applicationContext
+    private val verificationActivity: VerificationActivity = VerificationActivity()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+
+        loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        loginBinding.btnLoginInsert.setOnClickListener { view ->
+            auth.createUserWithEmailAndPassword(loginBinding.etEmailFormField.text.toString(), loginBinding.etPasswordFormField.text.toString())
+                .addOnSuccessListener {
+                    val snackbar: Snackbar =
+                        Snackbar.make(view, "계정이 정상적으로 등록되었습니다", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+
+                    intentFilter.getIntent(context, verificationActivity)
+                }
+        }
     }
 }
---
+++ app/src/main/java/com/hyun/worldwiser/ui/login/verification/VerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.worldwiser.ui.login.verification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyun.worldwiser.R
+
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vertification)
+    }
+}
