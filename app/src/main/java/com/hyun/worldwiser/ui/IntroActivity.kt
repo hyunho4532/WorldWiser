b661dec7a0f26c044861d4599cf2c033843eefb4
hyunho4532 <kyounguk192@gmail.com>
2023-12-23 16:32:26 +0900

    2023-12-23 스플래시 페이지 구현 중

--- app/src/main/java/com/hyun/worldwiser/ui/IntroActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/IntroActivity.kt
@@ -4,20 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import com.hyun.worldwiser.R
+import com.hyun.worldwiser.databinding.ActivityIntroBinding
+import com.hyun.worldwiser.type.AuthType
+import com.hyun.worldwiser.util.LoadAnimation
+import kotlinx.coroutines.*
 
+@DelicateCoroutinesApi
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var activityIntroBinding: ActivityIntroBinding
+    private var authType = AuthType.UnAuthStatus
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        activityIntroBinding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        activityIntroBinding.tvIntroStatus.text = "로딩 중"
+
+        GlobalScope.launch {
+            delay(2000L)
+
+            withContext(Dispatchers.Main) {
+                if (authType == AuthType.UnAuthStatus) {
+                    activityIntroBinding.tvIntroStatus.text = "로그인 페이지로 이동 중"
+                } else {
+                    activityIntroBinding.tvIntroStatus.text = "사용자 계정 불러오는 중"
+                }
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
-        val ivIntroAirplane = findViewById<ImageView>(R.id.iv_intro_airplane)
-        val animation = AnimationUtils.loadAnimation(this, R.anim.activtiy_intro_airplane_alpha)
-
-        ivIntroAirplane.startAnimation(animation)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.activity_intro_airplane_alpha)
+        activityIntroBinding.ivIntroAirplane.startAnimation(animation)
     }
 }
---
+++ app/src/main/java/com/hyun/worldwiser/util/LoadAnimation.kt
@@ -0,0 +1,14 @@
+package com.hyun.worldwiser.util
+
+import android.content.Context
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.hyun.worldwiser.R
+
+class LoadAnimation {
+    fun startAnimate(ivIntroAirplane: ImageView, context: Context, activityIntroAirplaneAlpha: Int) {
+        val animation = AnimationUtils.loadAnimation(context, R.anim.activity_intro_airplane_alpha)
+
+        ivIntroAirplane.startAnimation(animation)
+    }
+}
