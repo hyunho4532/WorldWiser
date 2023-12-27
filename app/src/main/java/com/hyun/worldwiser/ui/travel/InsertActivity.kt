f920b9f48e90f47dbaaea6f37d81bb2821a69230
hyunho4532 <kyounguk192@gmail.com>
2023-12-27 17:31:30 +0900

    2023-12-25 여행 일정 등록 액티비티 구현 중

--- app/src/main/java/com/hyun/worldwiser/ui/travel/InsertActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/travel/InsertActivity.kt
@@ -2,10 +2,28 @@ package com.hyun.worldwiser.ui.travel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.hyun.worldwiser.R
 
 class InsertActivity : AppCompatActivity() {
+
+    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert)
+
+        db.collection("verifications").document(auth.currentUser!!.uid).get()
+            .addOnSuccessListener { document ->
+                val nickname = document["nickname"].toString()
+
+                Log.d("InsertActivity", nickname)
+            }
+            .addOnFailureListener {
+
+            }
     }
 }
