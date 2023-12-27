1c61c840c5e21f7bc807835e7bae8888adaa87e4
hyunho4532 <kyounguk192@gmail.com>
2023-12-27 17:51:11 +0900

    2023-12-25 여행 일정 등록 액티비티 구현 중 (가고 싶은 나라 등록 구현 중)

--- app/src/main/java/com/hyun/worldwiser/ui/travel/InsertActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/travel/InsertActivity.kt
@@ -3,6 +3,7 @@ package com.hyun.worldwiser.ui.travel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.hyun.worldwiser.R
@@ -16,11 +17,13 @@ class InsertActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert)
 
+        val tvNicknameAuthWhoTravel: TextView = findViewById(R.id.tv_nickname_auth_who_travel)
+
         db.collection("verifications").document(auth.currentUser!!.uid).get()
             .addOnSuccessListener { document ->
                 val nickname = document["nickname"].toString()
 
-                Log.d("InsertActivity", nickname)
+                tvNicknameAuthWhoTravel.text = nickname + "님의 새로운 여행!"
             }
             .addOnFailureListener {
 
