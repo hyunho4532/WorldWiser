0a04a94e9cf802d52603d2969ccf3fd3cf16746d
hyunho4532 <kyounguk192@gmail.com>
2023-12-27 17:03:15 +0900

    2023-12-25 여행 일정 등록 액티비티 구현 중

---
+++ app/src/main/java/com/hyun/worldwiser/ui/travel/InsertActivity.kt
@@ -0,0 +1,11 @@
+package com.hyun.worldwiser.ui.travel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert)
+    }
+}
