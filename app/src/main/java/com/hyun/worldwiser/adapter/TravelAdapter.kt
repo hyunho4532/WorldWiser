646f8b185749145b965dd8fd3ccd7fd2e93f78f2
hyunho4532 <kyounguk192@gmail.com>
2023-12-30 19:50:54 +0900

    2023-12-29 일정 등록 액티비티 구현 중

--- app/src/main/java/com/hyun/worldwiser/adapter/TravelAdapter.kt
+++ app/src/main/java/com/hyun/worldwiser/adapter/TravelAdapter.kt
@@ -1,6 +1,7 @@
 package com.hyun.worldwiser.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.hyun.worldwiser.R
 import com.hyun.worldwiser.model.Travel
+import com.hyun.worldwiser.ui.schedule.ScheduleActivity
 
 class TravelAdapter(val context: Context, private val travelList: ArrayList<Travel>) : RecyclerView.Adapter<TravelAdapter.ViewHolder>() {
 
@@ -18,6 +20,11 @@ class TravelAdapter(val context: Context, private val travelList: ArrayList<Trav
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(travelList[position])
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ScheduleActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
---
+++ app/src/main/java/com/hyun/worldwiser/ui/schedule/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.worldwiser.ui.schedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyun.worldwiser.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
