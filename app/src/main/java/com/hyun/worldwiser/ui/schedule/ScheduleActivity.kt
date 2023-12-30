ae0396ae111944c18cdaa333c17b7c9368b0f8ed
hyunho4532 <kyounguk192@gmail.com>
2023-12-30 21:32:41 +0900

    2023-12-29 일정 등록 액티비티 구현 중

--- app/src/main/java/com/hyun/worldwiser/ui/schedule/ScheduleActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/schedule/ScheduleActivity.kt
@@ -1,10 +1,13 @@
 package com.hyun.worldwiser.ui.schedule
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.hyun.worldwiser.R
@@ -15,6 +18,9 @@ class ScheduleActivity : AppCompatActivity() {
     private val auth = FirebaseAuth.getInstance()
     private lateinit var context: Context
 
+    private lateinit var country: String
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
@@ -23,12 +29,18 @@ class ScheduleActivity : AppCompatActivity() {
 
         context = applicationContext
 
+        val bottomSheetTravelScheduleView = layoutInflater.inflate(R.layout.dialog_bottom_sheet_travel_schedule, null)
+
+        val bottomSheetTravelScheduleDialog = BottomSheetDialog(this)
+
+        bottomSheetTravelScheduleDialog.setContentView(bottomSheetTravelScheduleView)
+
         db.collection("travelInserts").whereEqualTo("authUid", auth.currentUser!!.uid).whereEqualTo("country", travelCountryIntent).get()
             .addOnSuccessListener { querySnapshot ->
 
                 for (document in querySnapshot.documents) {
                     val imageUrl = document["imageUrl"].toString()
-                    val country = document["country"].toString()
+                    country = document["country"].toString()
                     val startDay = document["startDay"].toString()
                     val endDay = document["endDay"].toString()
 
@@ -40,6 +52,18 @@ class ScheduleActivity : AppCompatActivity() {
                         .into(findViewById(R.id.iv_travel_schedule_url))
                 }
 
+            }
+
+        db.collection("verifications").document(auth.currentUser!!.uid).get()
+            .addOnSuccessListener { document ->
+
+                val nickname = document["nickname"].toString()
+
+                findViewById<AppCompatButton>(R.id.btn_schedule_insert).setOnClickListener {
+                    bottomSheetTravelScheduleDialog.show()
+
+                    bottomSheetTravelScheduleView.findViewById<TextView>(R.id.tv_nickname_auth_schedule).text = nickname + "님, " + country + "의 일정을 \n작성해주세요"
+                }
             }
     }
 }
