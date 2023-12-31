e328585c3197d0621bf3118a1de157f5cc07f5b0
hyunho4532 <kyounguk192@gmail.com>
2024-01-01 01:10:09 +0900

    2023-12-29 일정 등록 액티비티 구현 중 (일정 등록 완료)

--- app/src/main/java/com/hyun/worldwiser/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/hyun/worldwiser/adapter/ScheduleAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.hyun.worldwiser.R
 import com.hyun.worldwiser.model.Schedule
@@ -25,7 +26,9 @@ class ScheduleAdapter(val context: Context, private val scheduleList: ArrayList<
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(schedule: Schedule) {
+            val todo = schedule.todo
 
+            itemView.findViewById<TextView>(R.id.tv_travel_schedule_todo).text = todo
         }
     }
 }
--- app/src/main/java/com/hyun/worldwiser/ui/schedule/ScheduleActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/schedule/ScheduleActivity.kt
@@ -5,19 +5,27 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.hyun.worldwiser.R
+import com.hyun.worldwiser.adapter.ScheduleAdapter
+import com.hyun.worldwiser.adapter.TravelAdapter
 import com.hyun.worldwiser.decorator.DayDecorator
 import com.hyun.worldwiser.decorator.SaturdayDecorator
 import com.hyun.worldwiser.decorator.SundayDecorator
+import com.hyun.worldwiser.model.Schedule
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.lang.NullPointerException
 import java.util.*
 import java.util.Calendar.getInstance
+import kotlin.collections.ArrayList
 
 class ScheduleActivity : AppCompatActivity() {
 
@@ -25,9 +33,11 @@ class ScheduleActivity : AppCompatActivity() {
     private val auth = FirebaseAuth.getInstance()
     private lateinit var context: Context
 
+    private var scheduleList: ArrayList<Schedule> = ArrayList()
+
     private lateinit var country: String
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
@@ -68,30 +78,53 @@ class ScheduleActivity : AppCompatActivity() {
 
             }
 
+        db.collection("plans").whereEqualTo("authUid", auth.currentUser!!.uid).whereEqualTo("country", travelCountryIntent).get()
+            .addOnSuccessListener { querySnapshot ->
+
+                for (document in querySnapshot.documents) {
+                    scheduleList.add(Schedule(document["todo"].toString()))
+
+                    val recyclerView: RecyclerView = bottomSheetTravelScheduleView.findViewById(R.id.recyclerView)
+
+                    val scheduleAdapter = ScheduleAdapter(context, scheduleList)
+
+                    recyclerView.adapter = scheduleAdapter
+                    recyclerView.layoutManager = LinearLayoutManager(this)
+
+                    scheduleAdapter.notifyDataSetChanged()
+                }
+
+            }
+
         db.collection("verifications").document(auth.currentUser!!.uid).get()
             .addOnSuccessListener { document ->
 
                 val nickname = document["nickname"].toString()
 
                 findViewById<AppCompatButton>(R.id.btn_schedule_insert).setOnClickListener {
-                    bottomSheetTravelScheduleDialog.show()
 
-                    bottomSheetTravelScheduleView.findViewById<TextView>(R.id.tv_nickname_auth_schedule).text = nickname + "님! \n" + country + "의 일정을 작성해주세요"
+                    try {
+                        bottomSheetTravelScheduleDialog.show()
 
-                    bottomSheetTravelScheduleView.findViewById<AppCompatButton>(R.id.btn_schedule_datePicker_insert).setOnClickListener {
+                        bottomSheetTravelScheduleView.findViewById<TextView>(R.id.tv_nickname_auth_schedule).text = nickname + "님! \n" + country + "의 일정을 작성해주세요"
+                        bottomSheetTravelScheduleView.findViewById<AppCompatButton>(R.id.btn_schedule_datePicker_insert).setOnClickListener {
 
-                        // 달력 인스턴스 생성
-                        val calendar: Calendar = getInstance()
+                            val schedule = hashMapOf(
+                                "authUid" to auth.currentUser!!.uid,
+                                "country" to country,
+                                "todo" to bottomSheetTravelScheduleView.findViewById<EditText>(R.id.et_travel_schedule_todo).text.toString()
+                            )
 
-                        // 년도, 월, 일 가져오기
-                        var sYear = calendar.get(Calendar.YEAR)
-                        var sMonth = calendar.get(Calendar.MONTH)
-                        var sDay = calendar.get(Calendar.DAY_OF_MONTH)
+                            db.collection("plans").add(schedule)
+                                .addOnSuccessListener {
 
-                        val travelScheduleDate = bottomSheetTravelScheduleDatePickerView.findViewById<DatePicker>(R.id.datePicker)
+                                }
+                                .addOnFailureListener {
 
-
-                        bottomSheetTravelScheduleDatePickerDialog.show()
+                                }
+                        }
+                    } catch (e: NullPointerException) {
+                        print(e.printStackTrace())
                     }
                 }
             }
