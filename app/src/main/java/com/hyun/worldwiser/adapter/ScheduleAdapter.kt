6779c2dc18293c85d0a60754a0fa006e086d561e
hyunho4532 <kyounguk192@gmail.com>
2024-01-12 00:44:59 +0900

    2024-01-11 여행 등록 다이얼로그 UI 수정 중

--- app/src/main/java/com/hyun/worldwiser/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/hyun/worldwiser/adapter/ScheduleAdapter.kt
@@ -1,15 +1,20 @@
 package com.hyun.worldwiser.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.hyun.worldwiser.R
 import com.hyun.worldwiser.model.Schedule
 
 class ScheduleAdapter(val context: Context, private val scheduleList: ArrayList<Schedule>) : RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
+
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_travel_schedule_list, parent, false)
