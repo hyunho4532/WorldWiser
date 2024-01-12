cbe830f7bf49151075a507a036dca29822c9455e
hyunho4532 <kyounguk192@gmail.com>
2024-01-12 22:57:01 +0900

    2024-01-12 여행 추천 기능 구현 중 (Activity UI 작업 중)

---
+++ app/src/main/java/com/hyun/worldwiser/ui/travel/RecommendActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.worldwiser.ui.travel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyun.worldwiser.R
+
+class RecommendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommend)
+    }
+}
