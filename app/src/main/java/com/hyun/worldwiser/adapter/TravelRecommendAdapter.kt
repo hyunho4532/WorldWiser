3621ad76212faceeda4dfa6b2b1b375e8cbf8118
hyunho4532 <kyounguk192@gmail.com>
2024-01-14 22:08:58 +0900

    2024-01-14 여행 추천 기능 구현 중 (TravelRecommendAdapter 구현 완료 or Adapter 연결 완료)

--- app/src/main/java/com/hyun/worldwiser/adapter/TravelRecommendAdapter.kt
+++ app/src/main/java/com/hyun/worldwiser/adapter/TravelRecommendAdapter.kt
@@ -1,26 +1,35 @@
 package com.hyun.worldwiser.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.hyun.worldwiser.R
 import com.hyun.worldwiser.model.TravelRecommend
 
 class TravelRecommendAdapter(private val travelRecommendList: ArrayList<TravelRecommend>) :
     RecyclerView.Adapter<TravelRecommendAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_travel_recommend_list, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(travelRecommendList[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return travelRecommendList.size
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        fun bind(travelRecommend: TravelRecommend) {
+            val travelRecommendCountry = travelRecommend.travelRecommendCountry
+
+            itemView.findViewById<TextView>(R.id.tv_travel_recommend_country).text = travelRecommendCountry
+        }
     }
 }
--- app/src/main/java/com/hyun/worldwiser/ui/fragment/TravelFragment.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/fragment/TravelFragment.kt
@@ -6,11 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FirebaseFirestore
 import com.hyun.worldwiser.R
+import com.hyun.worldwiser.adapter.TravelAdapter
+import com.hyun.worldwiser.adapter.TravelRecommendAdapter
+import com.hyun.worldwiser.adapter.TravelSwipeToDeleteCallback
+import com.hyun.worldwiser.model.Travel
+import com.hyun.worldwiser.model.TravelRecommend
 import com.hyun.worldwiser.ui.travel.RecommendActivity
+import com.hyun.worldwiser.util.AdapterFilter
+import com.hyun.worldwiser.viewmodel.DateTimeFormatterViewModel
+import java.time.format.DateTimeFormatter
 
 class TravelFragment : Fragment() {
+
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val adapterFilter: AdapterFilter = AdapterFilter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +39,39 @@ class TravelFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_travel, container, false)
 
+        val travelRecommendList = ArrayList<TravelRecommend>()
+
         view.findViewById<FloatingActionButton>(R.id.fBtn_travel_recommend_insert).setOnClickListener {
             val intent = Intent(requireActivity(), RecommendActivity::class.java)
             startActivity(intent)
         }
+
+        val rvTravelRecommend = view.findViewById<RecyclerView>(R.id.rv_travel_recommend)
+
+        db.collection("travelRecommends").get()
+            .addOnSuccessListener { querySnapshot  ->
+
+                for (document in querySnapshot.documents) {
+
+                    try {
+                        val travelRecommendCountry = document["travelRecommendCountry"].toString()
+                        val travelRecommendAloneStatus = document["travelRecommendAloneStatus"].toString()
+                        val travelRecommendImpression = document["travelRecommendImpression"].toString()
+
+                        val travelRecommend = TravelRecommend(travelRecommendCountry, travelRecommendAloneStatus, travelRecommendImpression)
+                        travelRecommendList.add(travelRecommend)
+
+                        val travelRecommendAdapter = TravelRecommendAdapter(travelRecommendList)
+
+                        rvTravelRecommend.adapter = travelRecommendAdapter
+                        rvTravelRecommend.layoutManager = LinearLayoutManager(requireContext())
+
+
+                    } catch (e: UninitializedPropertyAccessException) {
+
+                    }
+                }
+            }
 
         return view
     }
