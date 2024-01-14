4bff9bc1328c26d9fa4ef94e4e878432af3f6e5e
hyunho4532 <kyounguk192@gmail.com>
2024-01-14 21:50:41 +0900

    2024-01-14 여행 추천 기능 구현 중 (TravelRecommendAdapter 구현 중)

---
+++ app/src/main/java/com/hyun/worldwiser/adapter/TravelRecommendAdapter.kt
@@ -0,0 +1,26 @@
+package com.hyun.worldwiser.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hyun.worldwiser.model.TravelRecommend
+
+class TravelRecommendAdapter(private val travelRecommendList: ArrayList<TravelRecommend>) :
+    RecyclerView.Adapter<TravelRecommendAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
