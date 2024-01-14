68b56735375bd41e7a1482349aa7746785989a28
hyunho4532 <kyounguk192@gmail.com>
2024-01-14 22:34:12 +0900

    2024-01-14 여행 추천 기능 구현 중 (fix: 갤러리에서 사진 가져오기 구현 중)

--- app/src/main/java/com/hyun/worldwiser/ui/travel/RecommendActivity.kt
+++ app/src/main/java/com/hyun/worldwiser/ui/travel/RecommendActivity.kt
@@ -1,12 +1,21 @@
 package com.hyun.worldwiser.ui.travel
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.hyun.worldwiser.Manifest
 import com.hyun.worldwiser.R
 import com.hyun.worldwiser.databinding.ActivityRecommendBinding
 import com.hyun.worldwiser.model.TravelRecommend
@@ -18,12 +27,41 @@ class RecommendActivity : AppCompatActivity() {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var activityRecommendBinding: ActivityRecommendBinding
     private val recommendTravelList: ArrayList<TravelRecommend>  = ArrayList()
+    private lateinit var imageView: ImageView
+    private var imageUri: Uri? = null
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted)
+                openGallery()
+        }
+
+    private val pickImageLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let {
+                    imageUri = it
+                    imageView.setImageURI(imageUri)
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityRecommendBinding = DataBindingUtil.setContentView(this, R.layout.activity_recommend)
 
         val verificationSelectViewModel: VerificationSelectViewModel = ViewModelProvider(this)[VerificationSelectViewModel::class.java]
+
+        activityRecommendBinding.ivTravelRecommendGallery.clipToOutline = true
+
+        activityRecommendBinding.ivTravelRecommendGallery.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
 
         verificationSelectViewModel.verificationNicknameSelectData { nickname ->
             activityRecommendBinding.tvNicknameAuthWhoTravelRecommend.setText(nickname + "님의 추천 여행지는?")
