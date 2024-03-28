d304ffd41054bb81b87a49abc7b1a7baaea79c60
hyunho4532 <kyounguk192@gmail.com>
2024-03-28 23:47:40 +0900

    2024-03-28 추천 여행지 Delete 기능 구현 중

--- app/src/main/java/com/hyun/worldwiser/FirebaseStorageManager.kt
+++ app/src/main/java/com/hyun/worldwiser/FirebaseStorageManager.kt
@@ -2,14 +2,12 @@ package com.hyun.worldwiser
 
 import android.graphics.Bitmap
 import android.net.Uri
-import android.os.storage.OnObbStateChangeListener
 import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.io.ByteArrayOutputStream
-import java.io.Console
 
 class FirebaseStorageManager {
     private val storage: FirebaseStorage = FirebaseStorage.getInstance()
--- app/src/main/java/com/hyun/worldwiser/service/RequestPermissionLauncher.kt
+++
@@ -1,5 +0,0 @@
-package com.hyun.worldwiser.service
-
-class RequestPermissionLauncher {
-
-}
