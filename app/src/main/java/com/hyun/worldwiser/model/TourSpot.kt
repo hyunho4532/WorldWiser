6c7824b9f51cbe0cd8a9dbfbcd0ce453d4941212
hyunho4532 <kyounguk192@gmail.com>
2024-04-12 20:25:35 +0900

    2024-04-12 메인 페이지 관광지 리스트 구현 중

---
+++ app/src/main/java/com/hyun/worldwiser/model/TourSpot.kt
@@ -0,0 +1,7 @@
+package com.hyun.worldwiser.model
+
+data class TourSpot (
+    val name: String,
+    val description: String,
+    val imageUrl: String
+)
---
+++ app/src/main/java/com/hyun/worldwiser/service/TourApiService.kt
@@ -0,0 +1,4 @@
+package com.hyun.worldwiser.service
+
+interface TourApiService {
+}
